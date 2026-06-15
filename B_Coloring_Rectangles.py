7fe6bec5dc7400b74a84f93e1887ebe771018bbf
GizawAAiT <gizawdagnemogessie@gmail.com>
2026-06-15 23:06:52 +0300

    start of June 15Th

--- B_Coloring_Rectangles.py
+++ B_Coloring_Rectangles.py
@@ -1,5 +1,7 @@
 def min_number_of_blue_cell_to_color_the_rectangle(n: int, m: int) -> int:
-    return min((n+2)//3 * m, (m+2)//3 * n)
+    option_1 = n*(m//3) + (m%3)*(n+2)//3
+    option_2 = m*(n//3) + (n%3)*(m+2)//3
+    return min(option_1, option_2)
 
 if __name__ == '__main__':
     for t in range(int(input())):
